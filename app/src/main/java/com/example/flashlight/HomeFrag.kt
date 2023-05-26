6f0925d12279c6845f74bb039ceda0b9d2822d2d
Sohaib <mohdsohaib78692@gmail.com>
2023-05-27 01:49:02 +0530

    Project Upload

---
+++ app/src/main/java/com/example/flashlight/HomeFrag.kt
@@ -0,0 +1,46 @@
+package com.example.flashlight
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.flashlight.databinding.FragmentHomeBinding
+
+
+class HomeFrag : Fragment() {
+    private var _binding : FragmentHomeBinding ?= null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        binding.btnOn.visibility = View.INVISIBLE
+        binding.imgLightOn.visibility = View.INVISIBLE
+        binding.txtTorchOn.visibility = View.INVISIBLE
+
+        binding.btnOff.setOnClickListener {
+            binding.btnOff.visibility = View.INVISIBLE
+            binding.btnOn.visibility = View.VISIBLE
+            binding.imgLightOff.visibility = View.INVISIBLE
+            binding.imgLightOn.visibility = View.VISIBLE
+            binding.txtTorchOff.visibility = View.INVISIBLE
+            binding.txtTorchOn.visibility = View.VISIBLE
+        }
+
+        binding.btnOn.setOnClickListener {
+            binding.btnOn.visibility = View.INVISIBLE
+            binding.btnOff.visibility = View.VISIBLE
+            binding.imgLightOn.visibility = View.INVISIBLE
+            binding.imgLightOff.visibility = View.VISIBLE
+            binding.txtTorchOn.visibility = View.INVISIBLE
+            binding.txtTorchOff.visibility = View.VISIBLE
+        }
+        return binding.root
+
+    }
+
+}
---
+++ app/src/main/java/com/example/flashlight/SplashScreen.kt
@@ -0,0 +1,50 @@
+package com.example.flashlight
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import com.example.flashlight.databinding.ActivitySplashScreenBinding
+import kotlin.time.Duration.Companion.milliseconds
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    private var _binding : ActivitySplashScreenBinding ? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        )
+
+        binding.imgTorchLight.visibility = View.GONE
+        binding.imgTextFlashLight.visibility = View.GONE
+
+        binding.imgTorchLight.postDelayed({
+            binding.imgTorchLight.visibility = View.VISIBLE
+        },2000)
+        binding.imgTextFlashLight.postDelayed({
+            binding.imgTextFlashLight.visibility = View.VISIBLE
+        },2000)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
